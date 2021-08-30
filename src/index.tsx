d698f4d86d5fcb3311f71fbdbaa7a9a3d000d028
alena zakharova <zakharova.alena@itmh.ru>
2021-08-30 12:18:34 +0500

    redux settings

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { rootReducer } from './services/reducers';
+import thunk from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose; 
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(rootReducer, enhancer); 
 
 ReactDOM.render(
   <React.StrictMode>
